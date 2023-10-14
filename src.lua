f155da812cba92125367be27f65c431cf38734eb
Moonzy <106113415+SpyTYX@users.noreply.github.com>
2023-10-14 23:57:29 +0100

    update src

--- src.lua
+++ src.lua
@@ -1,30 +1,29 @@
-local isWindows = package.config:sub(1, 1) == '\\';
-local ___ = "";
-local ____ = 6
+local isWindows = package.config:sub(1, 1) == '\\'
+local x = ""
+local y = 6
 
-function __()
-    local rand = math.random(1,2);return rand == 1 and string.char(math.random(65,90)) or tostring(math.random(0, 9))
+function z()
+    local rand = math.random(1, 2)
+    return rand == 1 and string.char(math.random(65, 90)) or tostring(math.random(0, 9))
 end
 
 if isWindows then
-    io.write("Enter the desired password length (4-25): ")
-    ____ = tonumber(io.read("*n")) or ____
+    io.write("Enter your password length to continue: ")
+    y = tonumber(io.read("*n")) or y
 
-    if ____ < 4 then
-        os.execute('Sorry the minimum password length is 4, your password legnth has been set to 4 automatically.')
-        ____ = 4
-    elseif ____ > 25 then
-        os.execute('Sorry the maximum password length is 25, your password legnth has been set to 25 automatically.')
-        ____ = 25
+    if y < 4 then
+        print("Sorry, the minimum password length is 4, your password length has been set to 4 automatically.")
+        y = 4
+    elseif y > 25 then
+        print("Sorry, the maximum password length is 25, your password length has been set to 25 automatically.")
+        y = 25
     end
-end
 
-for i = 1, ____ do
-    ___=___..__()
-end
-
-if isWindows then
-    os.execute('msg * "' .. "Your generated password is: " .. ___ .. '"')
+    for i = 1, y do
+        x = x .. z()
+    end
+    print("Your generated password is: " .. x .. "\nMade by .moonzy_ on Discord.")
+    os.execute("pause")
 else
     return
 end
